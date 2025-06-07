5991eaf64b0a8960554f6bf60e274e796af6627d
Hydra8985 <steve@dans-rac.com>
2025-06-07 15:08:34 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PartSearchApp from './PartSearchApp';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <PartSearchApp />
-  </React.StrictMode>
-);
